0d31d75296dafd60f3b1ce4a55f653edefae5db0
BhuvneshKumarAnand <bharatanand874@gmail.com>
2026-02-12 21:58:10 +0530

    First Occurrence in a String

---
+++ src/StringsPrep/StringsPrep.java
@@ -0,0 +1,70 @@
+package StringsPrep;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+
+public class StringsPrep {
+
+    /*
+    * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+    * determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+    * */
+    public static boolean isValid(String str) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                if ((stack.peek() == '(' && c == ')') ||
+                        (stack.peek() == '[' && c == ']') ||
+                        (stack.peek() == '{' && c == '}')
+                ) {
+                    stack.pop();
+                }else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /*
+    * Find the Index of the First Occurrence in a String
+    *
+    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+    * or -1 if needle is not part of haystack.
+    Example 1:
+
+    Input: haystack = "sadbutsad", needle = "sad"
+    Output: 0
+    Explanation: "sad" occurs at index 0 and 6.
+    The first occurrence is at index 0, so we return 0.
+    Example 2:
+
+    Input: haystack = "leetcode", needle = "leeto"
+    Output: -1
+    Explanation: "leeto" did not occur in "leetcode", so we return -1.
+    * */
+
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

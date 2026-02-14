bae94a1e9634559271c5e2ad49a00a75fdc7155f
BhuvneshKumarAnand <bharatanand874@gmail.com>
2026-02-14 23:50:20 +0530

    Add two string numbers

--- src/StringsPrep/StringsPrep.java
+++ src/StringsPrep/StringsPrep.java
@@ -411,6 +411,55 @@ Every close bracket has a corresponding open bracket of the same type.
                 ch == 'u' || ch == 'U';
     }
 
+    /*
+    * Add Strings
+    *
+    Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+    You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+
+
+    Example 1:
+
+    Input: num1 = "11", num2 = "123"
+    Output: "134"
+    Example 2:
+
+    Input: num1 = "456", num2 = "77"
+    Output: "533"
+    Example 3:
+
+    Input: num1 = "0", num2 = "0"
+    Output: "0"
+
+
+    Constraints:
+
+    1 <= num1.length, num2.length <= 104
+    num1 and num2 consist of only digits.
+    num1 and num2 don't have any leading zeros except for the zero itself.
+    * */
+
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i1 = num1.length() - 1;
+        int i2 = num2.length() - 1;
+        int carry = 0;
+        while (i1 >= 0 || i2 >= 0  || carry != 0) {
+            int d1 = (i1 >= 0) ? num1.charAt(i1) - '0' : 0;
+            int d2 = (i2 >= 0) ? num2.charAt(i2) - '0' : 0;
+
+            int sum = d1 + d2 + carry;
+            carry = sum / 10;
+            sb.append(sum % 10);
+
+            i1--;
+            i2--;
+        }
+        return sb.reverse().toString();
+    }
+
 
     public static void main(String[] args) {
         System.out.println(reverseVowels("leetcode"));

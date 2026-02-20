69d503305db0b20431362ebe5e84784c8bdb70b0
BhuvneshKumarAnand <bharatanand874@gmail.com>
2026-02-20 10:59:59 +0530

    Search Insert Position problem in Sorted Array

--- src/ArrayPrep/ArrayPrep.java
+++ src/ArrayPrep/ArrayPrep.java
@@ -639,11 +639,60 @@ Similarly, we can see at index 3 and 5, the elements to its left sum up to 15 an
         return i;
     }
 
+    /*
+    * Search Insert Position
+    Given a sorted array of distinct integers and a target value, return the index if the target is found.
+    * If not, return the index where it would be if it were inserted in order.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+
+
+    Example 1:
+
+    Input: nums = [1,3,5,6], target = 5
+    Output: 2
+    Example 2:
+
+    Input: nums = [1,3,5,6], target = 2
+    Output: 1
+    Example 3:
+
+    Input: nums = [1,3,5,6], target = 7
+    Output: 4
+
+
+    Constraints:
+
+    1 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    nums contains distinct values sorted in ascending order.
+    -104 <= target <= 104
+    * */
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }else if (nums[mid] < target) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
 
 
     public static void main(String[] args) {
         int[] arr1 = {3,2,2,3};
         int[] arr2 = {0,1,2,2,3,0,4,2};
+        int[] arr3 = {1,3,5,6};
 //        int[] out = new int[arr1.length];
 
 //        sumOfTwoArrays(arr1, arr2, out);
@@ -653,7 +702,6 @@ Similarly, we can see at index 3 and 5, the elements to its left sum up to 15 an
 
 //        System.out.print(Arrays.toString(arr2));
 //        System.out.println(removeElement(arr2, 2));
-        System.out.println(removeElement(arr1, 3));
-//        System.out.print(Arrays.toString(arr2));
+        System.out.println(searchInsert(arr3, 5));
     }
 }

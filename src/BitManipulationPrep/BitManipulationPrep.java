283e7f05ece6da37096cd909b18919ad73472bef
BhuvneshKumarAnand <bharatanand874@gmail.com>
2026-03-03 10:15:09 +0530

    Single umber Problem : (using Bit Manipulation)

--- src/BitManipulationPrep/BitManipulationPrep.java
+++ src/BitManipulationPrep/BitManipulationPrep.java
@@ -37,8 +37,25 @@ public class BitManipulationPrep {
         return (n & 0x55555555) != 0;
     }
 
+    /*
+    * Single Number
+    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+    You must implement a solution with a linear runtime complexity and use only constant extra space.
+    * */
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(isPowerOfFour(8));
+        int[] nums = {2,2,1};
+        System.out.println(singleNumber(nums));
     }
 }

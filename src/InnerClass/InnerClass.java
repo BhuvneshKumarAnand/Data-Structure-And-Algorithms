77aa77ab6edc2348c898979c02de09d13ca61703
BhuvneshKumarAnand <bharatanand874@gmail.com>
2026-06-27 01:12:43 +0530

    Anonymous Inner Class

--- src/InnerClass/InnerClass.java
+++ src/InnerClass/InnerClass.java
@@ -3,12 +3,20 @@ package InnerClass;
 public class InnerClass {
     public static void main(String[] args) {
         A obj = new A();
-        obj.show();
+        // obj.show();
 
         A.B newObj = obj.new B(); // instantiate with the object of parent class
-        newObj.config();
+        // newObj.config();
 
         A.C obj1 = new A.C(); // static inner class can be instantiated without the object of parent class
-        obj1.show();
+        // obj1.show();
+
+        A obj2 = new A(){
+            @Override
+            public void show(){
+                System.out.println("In show 2...");
+            }
+        };
+        obj2.show();
     }
 }
